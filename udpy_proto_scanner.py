35684778a088d6e1b1a538479070edeacc04510c
Mark Lowe <mark-lowe@users.noreply.github.com>
2023-07-28 15:09:01 +0100

    Update udpy_proto_scanner.py

--- udpy_proto_scanner.py
+++ udpy_proto_scanner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # udpy_proto_scanner - UDP Service Discovery Tool
-# Copyright (C) 2023  Mark Lowe
+# Copyright Cisco Systems, Inc. and its affiliates
 #
 # This tool may be used for legal purposes only.  Users take full responsibility
 # for any actions performed using this tool.  The author accepts no liability
